a74836e38564168e0fb94c0d828dbd469cbee100
Govardhan-018 <govardhanhegde05@gmail.com>
2025-05-21 00:33:33 +0530

    Main part done

--- home_patient/index.js
+++ home_patient/index.js
@@ -1,19 +1,43 @@
 import express from "express"
 import axios from "axios"
 
-const patient_data="http://localhost:3999/getinfo"
-const app=express()
+const patient_data = "http://localhost:3999"
+const app = express()
 app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use(express.static("public"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/",async(req,res)=>{
-    const id=req.query.id
-    const response = await axios.post(`${patient_data}?pid=${id}`)
-    res.send(response.data)
+app.get("/", async (req, res) => {
+    const id = req.query.id
+    const response = await axios.post(`${patient_data}/getinfo?pid=${id}`)
+    res.render("index.ejs", {
+        data: response.data,
+        id: id
+    })
 })
-app.listen(4040,(req,res)=>{
+app.post("/new_apoint", async (req, res) => {
+    const response = await axios.get(`${patient_data}/doctors`)
+    const id = req.body.id
+    res.render("index2.ejs", {
+        pid: id,
+        doctor: response.data
+    })
+})
+app.post("/creat_apoint", async (req, res) => {
+    const doctor_id = req.body.doctor
+    const patient_id = req.body.id
+    const discription = req.body.discription
+    const url = `${patient_data}/creat?pid=${patient_id}&doctor=${doctor_id}&discription=${discription}`
+    try {
+        await axios.post(url);
+        return res.redirect(`/?id=${patient_id}`);
+    } catch (err) {
+        console.error(err.response?.data);
+        return res.status(401).json({ message: "Something went wrong" });
+    }
+})
+app.listen(4040, (req, res) => {
 
 })
--- patient_data/index.js
+++ patient_data/index.js
@@ -12,17 +12,40 @@ const db = new pg.Client({
 })
 db.connect()
 
-app.post("/getinfo",async(req,res)=>{
-const patient_id=req.query.pid
-const query1=`SELECT * FROM patients_data WHERE patient_id=${patient_id}`
-try{
-const data=await db.query(query1)
-res.status(200).json(data.rows)
-}catch(err){
-res.status(401).json({ message: "Invalid patient id"})
-}
+app.post("/getinfo", async (req, res) => {
+    const patient_id = req.query.pid
+    const query1 = `SELECT * FROM patients_data WHERE patient_id=${patient_id}`
+    try {
+        const data = await db.query(query1)
+        res.status(200).json(data.rows)
+    } catch (err) {
+        res.status(401).json({ message: "Invalid patient id" })
+    }
 })
-
-app.listen(3999,(req,res)=>{
+app.post("/creat", async (req, res) => {
+    const doctor_id = req.query.doctor
+    const patient_id = req.query.pid
+    const discription = req.query.discription
+    const status = "WAITING"
+    const query1 = `INSERT INTO patients_data (patient_id, doctor_id, discription, status) VALUES ($1, $2, $3, $4)`;
+    try{
+    await db.query(query1, [patient_id, doctor_id, discription, status]);
+     console.log("fixed")
+    res.status(200).json({ message: "Appointment fixed" })
+    }catch(err){
+        console.log("Notfixed")
+         res.status(401).json({ message: "Appointment notfixed" })
+    }
+})
+app.get("/doctors", async (req, res) => {
+    const query1 = `SELECT name,id FROM doctor WHERE proxy='present'`
+    try {
+        const data = await db.query(query1)
+        res.status(200).json(data.rows)
+    } catch (err) {
+        res.status(401).json({ message: "Something went wrong" })
+    }
+})
+app.listen(3999, (req, res) => {
     console.log("On services...")
 })
